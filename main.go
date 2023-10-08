1c18d83d2c19ef5f95cb2ef029e70184ed98002d
periaate <daniel.saury.00@gmail.com>
2023-10-08 16:23:45 +0300

    Refactored code for readability, added comments, and embedded patterns.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"io/fs"
 	"log"
@@ -14,208 +15,176 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-const configFile = "config.yml"
+//go:embed patterns.yml
+var patternsFile []byte
 
-var conf extcfg
-var opts Options
+var conf patterns // Configuration variable.
+var opts Options  // Flag options.
 
+var (
+	inclusionMap = map[string]bool{}
+	exclusionMap = map[string]bool{}
+
+	highestTime int64                 // Highest found unix timestamp.
+	lowestTime  int64 = math.MaxInt64 // Lowest found unix timestamp. Uses MaxInt64 to make comparisons work.
+
+	fp = "."
+)
+
+// SortableFiles is an array of sortableFiles which is sortable.
+type SortableFiles []sortableFile
+
+func (s SortableFiles) Len() int           { return len(s) }
+func (s SortableFiles) Less(i, j int) bool { return s[i].Fp < s[j].Fp }
+func (s SortableFiles) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
+type sortableFile struct {
+	Fp    string // Filepath
+	Value int64  // Countable value to be used by counting sort. Populated by unix timestamp.
+}
+
+// Todo: Absolute path flag, Custom pattern files
 type Options struct {
-	Recurse bool `short:"r" long:"recurse" description:"Recursively list files in subdirectories"`
-
-	Ascending  bool `short:"A" long:"ascending" description:"Results will be ordered in ascending order."`
-	Descending bool `short:"D" long:"descending" description:"Results will be ordered in descending order (default)."`
-
-	Date bool `short:"d" long:"date" description:"Results will be ordered by their modified time."`
-	Name bool `short:"n" long:"name" description:"Results will be ordered by their names (default)."`
+	Recurse   bool `short:"r" long:"recurse" description:"Recursively list files in subdirectories"`
+	Ascending bool `short:"A" long:"ascending" description:"Results will be ordered in ascending order. Files are ordered into descending order by default."`
+	Date      bool `short:"d" long:"date" description:"Results will be ordered by their modified time. Files are ordered by filename by default"`
 
 	Include string `short:"i" long:"include" description:"Given an existing extension pattern configuration target, will include only items fitting the pattern. Use ',' to define multiple patterns."`
 	Exclude string `short:"e" long:"exclude" description:"Given an existing extension pattern configuration target, will excldue items fitting the pattern. Use ',' to define multiple patterns."`
 }
 
-type extcfg struct {
+type patterns struct {
 	Extensions map[string][]string `yaml:"extensions"`
 }
 
 func main() {
 	_, err := gf.Parse(&opts)
 	if err != nil {
-		fmt.Println("Error parsing flags:", err)
-		os.Exit(1)
+		log.Fatalln("Error parsing flags:", err)
 	}
 
-	fp := "."
+	// Get filepath
 	if len(os.Args) > 1 && os.Args[1][0] != '-' {
 		fp = os.Args[1]
 	}
 
+	// Validate filepath
 	stat, err := os.Stat(fp)
 	if err != nil || !stat.IsDir() {
-		fmt.Println("Invalid directory")
-		os.Exit(1)
+		log.Fatalln(err)
 	}
 
-	inc := map[string]bool{}
-	exc := map[string]bool{}
+	err = yaml.Unmarshal(patternsFile, &conf)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	parsePatterns()
 
-	if opts.Include != "" {
+	files := SortableFiles{}
+	getFiles(&files)
 
-		incstr := strings.Split(opts.Include, ",")
+	if opts.Date {
+		files = countingSort(files)
+	} else {
+		sort.Sort(files)
+	}
 
-		for _, key := range incstr {
-			if v, ok := conf.Extensions[key]; ok {
-				for _, val := range v {
-					inc[val] = ok
-				}
+	printResults(files)
+}
+
+// getFiles attempts to populate the files array using the existing configurations.
+func getFiles(files *SortableFiles) {
+	// perEntry is ran on each file to construct sortableFile and check if it matches any patterns.
+	perEntry := func(d fs.DirEntry, path string) {
+		file := sortableFile{
+			Fp: filepath.Join(path, d.Name()),
+		}
+
+		if opts.Date {
+			inf, err := d.Info()
+			if err != nil {
+				return
 			}
+			unixTime := inf.ModTime().Unix()
+			updateTime(unixTime)
+			file.Value = unixTime
+		}
+
+		// Check files extension and match against patterns.
+		ext := filepath.Ext(file.Fp)
+		incl := true
+		if len(inclusionMap) != 0 {
+			if _, ok := inclusionMap[ext]; !ok {
+				incl = false
+			}
+		}
+		if len(exclusionMap) != 0 {
+			if _, ok := exclusionMap[ext]; ok {
+				incl = false
+			}
+		}
+
+		if incl {
+			*files = append(*files, file)
 		}
 	}
 
-	if opts.Exclude != "" {
-		excstr := strings.Split(opts.Exclude, ",")
-		for _, key := range excstr {
-			if v, ok := conf.Extensions[key]; ok {
-				for _, val := range v {
-					exc[val] = ok
-				}
-			}
-		}
-	}
-
-	ress := ByFp{}
-	var L int64
-	var S int64 = math.MaxInt64
-
+	// Get files
 	if !opts.Recurse {
 		res, err := os.ReadDir(fp)
 		if err != nil {
 			log.Fatalln(err)
 		}
 		for _, e := range res {
-			inf, err := e.Info()
-			if err != nil {
-				continue
-			}
-			dt := inf.ModTime().Unix()
-			if dt > L {
-				L = dt
-			}
-			if dt < S {
-				S = dt
-			}
-			ress = append(ress, sortableFile{
-				Fp:  fmt.Sprintf("%s/%s", fp, e.Name()),
-				Val: dt,
-			})
+			perEntry(e, fp)
 		}
 	} else {
-		vfs := os.DirFS(fp)
-		err := fs.WalkDir(vfs, fp, func(path string, d fs.DirEntry, _ error) error {
-			if d.IsDir() {
+		err := fs.WalkDir(os.DirFS(fp), fp, func(path string, e fs.DirEntry, _ error) error {
+			if e.IsDir() {
 				return nil
 			}
-			inf, err := d.Info()
-			if err != nil {
-				return nil
-			}
-			dt := inf.ModTime().Unix()
-			if dt > L {
-				L = dt
-			}
-			if dt < S {
-				S = dt
-			}
-			ress = append(ress, sortableFile{
-				Fp:  fmt.Sprintf("%s/%s", path, d.Name()),
-				Val: dt,
-			})
+			perEntry(e, path)
 			return nil
 		})
 		if err != nil {
 			log.Fatalln(err)
 		}
 	}
-
-	files := ByFp{}
-	for _, e := range ress {
-		ext := filepath.Ext(e.Fp)
-		incl := true
-		if len(inc) != 0 {
-			if _, ok := inc[ext]; !ok {
-				incl = false
-			}
-		}
-		if len(exc) != 0 {
-			if _, ok := exc[ext]; ok {
-				incl = false
-			}
-		}
-
-		if incl {
-			e.Val -= S
-			files = append(files, e)
-		}
-	}
-
-	if opts.Date {
-		files = CountingSort(files, int(L-S))
-	} else {
-		sort.Sort(files)
-	}
-
-	if opts.Ascending {
-		for i := range files {
-			f := files[len(files)-1-i]
-			fmt.Println(f.Fp)
-		}
-	} else {
-		for _, f := range files {
-			fmt.Println(f.Fp)
-		}
-	}
-
 }
 
-type ByFp []sortableFile
-
-func (s ByFp) Len() int {
-	return len(s)
-}
-
-func (s ByFp) Less(i, j int) bool {
-	return s[i].Fp < s[j].Fp
-}
-
-func (s ByFp) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-type sortableFile struct {
-	Fp  string
-	Val int64
-}
-
-func init() {
-	yml, err := os.ReadFile(configFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = yaml.Unmarshal(yml, &conf)
-	if err != nil {
-		log.Fatal(err)
+// printResults prints the results to stdout.
+func printResults(files SortableFiles) {
+	for i := range files {
+		k := i
+		if opts.Ascending {
+			k = len(files) - 1 - i
+		}
+		fmt.Println(files[k].Fp)
 	}
 }
 
-// CountingSort sorts an array into descending order.
+// updateTime updates the unix timestamp boundaries.
+func updateTime(t int64) {
+	if t > highestTime {
+		highestTime = t
+	}
+	if t < lowestTime {
+		lowestTime = t
+	}
+}
+
+// countingSort sorts an array into descending order.
 //
 // Counting sort needs to be called with an array, an integer k which is
 // the largest value in the array, and a function which takes an element
 // of the array as argument, and returns its value in the range [0, k].
-func CountingSort(input []sortableFile, k int) []sortableFile {
+func countingSort(input []sortableFile) []sortableFile {
+	k := int(highestTime - lowestTime)
 	count := make([]int, k+1)
 
 	// Count occurrences of each value.
 	for _, v := range input {
-		count[v.Val]++
+		count[v.Value-lowestTime]++
 	}
 
 	// Build and apply offset by summing the counts of previous values.
@@ -225,9 +194,41 @@ func CountingSort(input []sortableFile, k int) []sortableFile {
 
 	result := make([]sortableFile, len(input))
 	for _, v := range input {
-		result[len(input)-count[v.Val]] = v
-		count[v.Val]--
+		result[len(input)-count[v.Value-lowestTime]] = v
+		count[v.Value-lowestTime]--
 	}
 
 	return result
+}
+
+// parsePatterns checks for flags and builds pattern maps accordingly which will be used to
+// include or exclude files as per the pattern.
+func parsePatterns() {
+	if opts.Include != "" {
+		// Split flag arguments by comma for multiple items.
+		incstr := strings.Split(opts.Include, ",")
+
+		for _, key := range incstr {
+			// Find matching extensions and add each of their elements to the inclusion map.
+			if v, ok := conf.Extensions[key]; ok {
+				for _, val := range v {
+					inclusionMap[val] = ok
+				}
+			}
+		}
+	}
+
+	if opts.Exclude != "" {
+		// Split flag arguments by comma for multiple items.
+		excstr := strings.Split(opts.Exclude, ",")
+
+		for _, key := range excstr {
+			// Find matching extensions and add each of their elements to the exclusion map.
+			if v, ok := conf.Extensions[key]; ok {
+				for _, val := range v {
+					exclusionMap[val] = ok
+				}
+			}
+		}
+	}
 }
